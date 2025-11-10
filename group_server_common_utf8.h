53a12d17dabfab3d618217f4862be42fd6a44f5d
shimo-beep <c612401096@tokushima-u.ac.jp>
2025-11-10 13:52:47 +0900

    Add group_server_common_utf8.h for server constants
    ゲーム開発の基盤となるヘッダファイル(サーバ側)

---
+++ group_server_common_utf8.h
@@ -0,0 +1,13 @@
+/*****************************************************************
+ファイル名	: group_server_common_utf8.h
+機能		: サーバーで使用する定数の宣言を行う
+*****************************************************************/
+
+#ifndef _SERVER_COMMON_H_
+#define _SERVER_COMMON_H_
+
+#include"group_common_utf8.h"
+
+#define ALL_CLIENTS	-1   /* 全クライアントにデータを送る時に使用する */
+
+#endif

8cc0991693c3079159a890d66bdd3591b42ef9a0
shimo-beep <c612401096@tokushima-u.ac.jp>
2025-11-15 14:25:06 +0900

    Add commands for weapon selection and game start

--- common_utf8.h
+++ common_utf8.h
@@ -16,16 +16,18 @@
 #define PORT			(u_short)8888	/* ポート番号 */
 
 #define MAX_CLIENTS		4				/* クライアント数の最大値 */
+
 #define MAX_NAME_SIZE	10 				/* ユーザー名の最大値*/
 
 #define MAX_DATA		200				/* 送受信するデータの最大値 */
 
-/* 既存の定義群のどこかに追加 */
 #define X_COMMAND        'X'   /* クライアントがXキーを押したことをサーバへ通知 */
 #define UPDATE_X_COMMAND 'U'   /* サーバーが全クライアントへX押下情報を配信 */
 
 #define END_COMMAND		'E'		  		/* プログラム終了コマンド */
 #define NEXT_SCREEN_COMMAND 'N' /* 次の画面へ進むコマンド */
+#define SELECT_WEAPON_COMMAND 'W' /* クライアントが武器を選択したことを通知 */
+#define START_GAME_COMMAND 'S' /* サーバーがクライアントに武器選択画面へ移らせるコマンド */
 
 #define TRANSITION_TIMER_ID 100 /* 画面遷移時の遅延時間（ミリ秒） */
 

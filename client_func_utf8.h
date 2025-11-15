3f0077e7287b2166f47ec9df39d473fada622689
shimo-beep <c612401096@tokushima-u.ac.jp>
2025-11-15 14:23:24 +0900

    Update client_func_utf8.h with new function declarations
    Added declarations for new functions and updated screen state constants.

--- client_func_utf8.h
+++ client_func_utf8.h
@@ -14,12 +14,15 @@ extern void CloseSoc(void);
 extern int RecvIntData(int *intData);
 extern void SendData(void *data,int dataSize);
 extern int SendRecvManager(void);
-extern int RecvCharData(char *charData); // 追加部分
+extern int RecvCharData(char *charData); 
 
 /* client_win.c */
 extern int InitWindows(int clientID,int num,char name[][MAX_NAME_SIZE]);
 extern void DestroyWindow(void);
 extern void WindowEvent(int num);
+extern int gMyClientID;     // 自分のクライアント番号
+extern char gAllName[MAX_CLIENTS][MAX_NAME_SIZE]; // 全員の名前
+
 
 /* client_command.c */
 extern int ExecuteCommand(char command);
@@ -32,18 +35,13 @@ extern void SetMyClientID(int clientID);
 extern void SetIntData2DataBlock(void *data, int intData, int *dataSize);
 extern void SetCharData2DataBlock(void *data, char charData, int *dataSize);
 
-// client_func_utf8.h の末尾付近に追記
-
 // 画面の状態定数
-#define SCREEN_STATE_LOBBY 0        // 参加者リスト表示画面
-#define SCREEN_STATE_GAME_SCREEN 1  // ゲーム本編の画面（リスト非表示）
+#define SCREEN_STATE_LOBBY 0       
+#define SCREEN_STATE_GAME_SCREEN 1  
+#define SCREEN_STATE_RESULT 2
 
-// client_command.c の外部宣言
 extern int ExecuteCommand(char command);
-// ...
-
-// client_win.c 側の外部宣言（必要であれば）
-extern void SetScreenState(int state); // 【必須追加】この宣言を追加する
-extern void SetXPressedFlag(int clientID); // Xキー押下フラグ更新関数が既にあるはず
+extern void SetScreenState(int state);
+extern void SetXPressedFlag(int clientID); 
 
 #endif

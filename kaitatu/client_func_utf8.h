2a94957557c942c0b167633f03f1b1c24ec7080e
shimo-beep <c612401096@tokushima-u.ac.jp>
2025-12-04 23:12:04 +0900

    Add files via upload

---
+++ kaitatu/client_func_utf8.h
@@ -0,0 +1,52 @@
+/*client_func_utf8.h*/
+#ifndef _CLIENT_FUNC_H_
+#define _CLIENT_FUNC_H_
+#include"common_utf8.h"
+
+// 発射体の構造体
+typedef struct {
+    int x;
+    int y;
+    int clientID; // 誰が撃ったか
+    int active;   // 1:有効, 0:無効
+} Projectile;
+
+extern Projectile gProjectiles[MAX_PROJECTILES];
+
+extern void InitProjectiles(void);
+extern void UpdateAndDrawProjectiles(void);
+extern void SendFireCommand(void);
+/* client_net.c */
+extern int SetUpClient(char* hostName,int *clientID,int *num,char clientName[][MAX_NAME_SIZE]);
+extern void CloseSoc(void);
+extern int RecvIntData(int *intData);
+extern void SendData(void *data,int dataSize);
+extern int SendRecvManager(void);
+extern int RecvCharData(char *charData); 
+
+/* client_win.c */
+extern int InitWindows(int clientID,int num,char name[][MAX_NAME_SIZE]);
+extern void DestroyWindow(void);
+extern void WindowEvent(int num);
+extern void DrawImageAndText(void); // DrawImageAndTextの宣言
+extern int gMyClientID;     // 自分のクライアント番号
+extern char gAllName[MAX_CLIENTS][MAX_NAME_SIZE]; // 全員の名前
+extern int gPlayerHP[MAX_CLIENTS]; // ★ 全員のHP配列の外部宣言を維持 ★
+
+/* client_command.c */
+extern int ExecuteCommand(char command);
+extern void SendEndCommand(void);
+extern void SetMyClientID(int clientID);
+extern void SetIntData2DataBlock(void *data, int intData, int *dataSize);
+extern void SetCharData2DataBlock(void *data, char charData, int *dataSize);
+
+// 画面の状態定数
+#define SCREEN_STATE_LOBBY 0       
+#define SCREEN_STATE_GAME_SCREEN 1  
+#define SCREEN_STATE_RESULT 2
+extern int ExecuteCommand(char command);
+extern void SetScreenState(int state);
+extern void SetXPressedFlag(int clientID); 
+extern void UpdatePlayerPos(int clientID, char direction); // 座標更新ヘルパー関数
+extern void SetPlayerMoveStep(int clientID, int step); // 速度設定関数
+#endif
---
+++ kaitatu/client_net_utf8.c
@@ -0,0 +1,163 @@
+/*client_net_utf8.c*/
+
+#include"common_utf8.h"
+#include"client_func_utf8.h"
+#include<sys/socket.h>
+#include<netdb.h>
+#include<unistd.h>
+#define	BUF_SIZE	100
+
+static int	gSocket;	/* ソケット */
+static fd_set	gMask;	/* select()用のマスク */
+static int	gWidth;		/* gMask中ののチェックすべきビット数 */
+static void GetAllName(int *clientID,int *num,char clientNames[][MAX_NAME_SIZE]);
+static void SetMask(void);
+static int RecvData(void *data,int dataSize);
+
+int SetUpClient(char *hostName,int *clientID,int *num,char clientNames[][MAX_NAME_SIZE])
+{
+    struct hostent	*servHost;
+    struct sockaddr_in	server;
+    int			len;
+    char		str[BUF_SIZE];
+
+    /* ホスト名からホスト情報を得る */
+    if((servHost = gethostbyname(hostName))==NULL){
+		fprintf(stderr,"Unknown host\n");
+		return -1;
+    }
+
+    bzero((char*)&server,sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    bcopy(servHost->h_addr,(char*)&server.sin_addr,servHost->h_length);
+
+    /* ソケットを作成する */
+    if((gSocket = socket(AF_INET,SOCK_STREAM,0)) < 0){
+		fprintf(stderr,"socket allocation failed\n");
+		return -1;
+    }
+
+    /* サーバーと接続する */
+    if(connect(gSocket,(struct sockaddr*)&server,sizeof(server)) == -1){
+		fprintf(stderr,"cannot connect\n");
+		close(gSocket);
+		return -1;
+    }
+    fprintf(stderr,"connected\n");
+
+    /* 名前を読み込みサーバーに送る */
+    do{
+		printf("Enter Your Name\n");
+		fgets(str,BUF_SIZE,stdin);
+		len = strlen(str)-1;
+		str[len]='\0';
+    }while(len>MAX_NAME_SIZE-1 || len==0);
+    SendData(str,MAX_NAME_SIZE);
+    printf("Please Wait\n");
+
+    /* 全クライアントのユーザー名を得る */
+    GetAllName(clientID,num,clientNames);     
+    SetMyClientID(*clientID);
+
+    /* select()のためのマスク値を設定する */
+    SetMask();
+    
+    return 0;
+}
+
+int SendRecvManager(void)
+{
+    fd_set	readOK;
+    char	command;
+    int		i;
+    int		endFlag = 1;
+    struct timeval	timeout;
+
+    /* select()の待ち時間を設定する */
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 20;
+
+    readOK = gMask;
+    /* サーバーからデータが届いているか調べる */
+    select(gWidth,&readOK,NULL,NULL,&timeout);
+    if(FD_ISSET(gSocket,&readOK)){
+		/* サーバーからデータが届いていた */
+    	/* コマンドを読み込む */
+		RecvData(&command,sizeof(char));
+    	/* コマンドに対する処理を行う */
+		endFlag = ExecuteCommand(command);
+    }
+    return endFlag;
+}
+
+int RecvIntData(int *intData)
+{
+    int n,tmp;  
+    /* 引き数チェック */
+    assert(intData!=NULL);
+    n = RecvData(&tmp,sizeof(int));
+    (*intData) = ntohl(tmp);
+    return n;
+}
+
+int RecvCharData(char *charData)
+{
+    int n;
+    /* 引き数チェック */
+    assert(charData!=NULL);
+    // char型は1バイトなので、そのままRecvDataを呼び出す
+    n = RecvData(charData, sizeof(char));
+
+    return n;
+}
+
+void SendData(void *data,int dataSize)
+{
+    /* 引き数チェック */
+    assert(data != NULL);
+    assert(0 < dataSize);
+    write(gSocket,data,dataSize);
+}
+
+void CloseSoc(void)
+{
+    printf("...Connection closed\n");
+    close(gSocket);
+}
+
+static void GetAllName(int *clientID,int *num,char clientNames[][MAX_NAME_SIZE])
+{
+    int	i;
+    /* クライアント番号の読み込み */
+    RecvIntData(clientID);
+    /* クライアント数の読み込み */
+    RecvIntData(num);
+    /* 全クライアントのユーザー名を読み込む */
+    for(i=0;i<(*num);i++){
+		RecvData(clientNames[i],MAX_NAME_SIZE);
+    }
+#ifndef NDEBUG
+    printf("#####\n");
+    printf("client number = %d\n",(*num));
+    for(i=0;i<(*num);i++){
+		printf("%d:%s\n",i,clientNames[i]);
+    }
+#endif
+}
+
+static void SetMask(void)
+{
+    int	i;
+    FD_ZERO(&gMask);
+    FD_SET(gSocket,&gMask);
+    gWidth = gSocket+1;
+}
+
+int RecvData(void *data,int dataSize)
+{
+    /* 引き数チェック */
+    assert(data != NULL);
+    assert(0 < dataSize);
+    return read(gSocket,data,dataSize);
+}

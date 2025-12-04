0809928ba6272fd76bc4d5a79c6e817d10ebf7cb
shimo-beep <c612401096@tokushima-u.ac.jp>
2025-12-04 23:10:55 +0900

    Add files via upload

---
+++ kaitatu/server_func_utf8.h
@@ -0,0 +1,31 @@
+/*server_func_utf8.h*/
+#ifndef _SERVER_FUNC_H_
+#define _SERVER_FUNC_H_
+
+#include"server_common_utf8.h"
+
+/* CLIENT 型と配列の外部宣言 */
+typedef struct {
+    int fd;
+    char name[MAX_NAME_SIZE];
+} CLIENT;
+
+extern CLIENT gClients[MAX_CLIENTS];
+
+/* server_net.c */
+extern int SetUpServer(int num);
+extern void Ending(void);
+extern int RecvIntData(int pos,int *intData);
+extern int RecvCharData(int pos,char *charData); // ★ 追加: RecvCharDataの宣言
+extern void SendData(int pos,void *data,int dataSize);
+extern int SendRecvManager(void);
+extern int GetClientNum(void);
+
+/* server_command.c */
+extern int ExecuteCommand(char command,int pos);
+extern void SendDiamondCommand(void);
+extern void SendDataToClient(int clientID, const void *data, int dataSize);
+extern int gPlayerPosX[MAX_CLIENTS];
+extern int gPlayerPosY[MAX_CLIENTS];
+
+#endif
---
+++ kaitatu/server_net_utf8.c
@@ -0,0 +1,209 @@
+/*server_net_utf8.c*/
+
+#include"server_common_utf8.h"
+#include"server_func_utf8.h"
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<unistd.h>
+
+CLIENT	gClients[MAX_CLIENTS];	/* クライアント */
+static int	gClientNum;					/* クライアント数 */
+
+static fd_set	gMask;					/* select()用のマスク */
+static int	gWidth;						/* gMask中のチェックすべきビット数 */
+static int MultiAccept(int request_soc,int num);
+static void Enter(int pos, int fd);
+static void SetMask(int maxfd);
+static void SendAllName(void);
+static int RecvData(int pos,void *data,int dataSize);
+
+
+int SetUpServer(int num)
+{
+    struct sockaddr_in	server;
+    int			request_soc;
+    int                 maxfd;
+    int			val = 1;
+ 
+    /* 引き数チェック */
+    assert(0<num && num<=MAX_CLIENTS);
+
+    gClientNum = num;
+    bzero((char*)&server,sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    /* ソケットを作成する */
+    if((request_soc = socket(AF_INET,SOCK_STREAM,0)) < 0){
+		fprintf(stderr,"Socket allocation failed\n");
+		return -1;
+    }
+    setsockopt(request_soc,SOL_SOCKET,SO_REUSEADDR,&val,sizeof(val));
+
+    /* ソケットに名前をつける */
+    if(bind(request_soc,(struct sockaddr*)&server,sizeof(server))==-1){
+		fprintf(stderr,"Cannot bind\n");
+		close(request_soc);
+		return -1;
+    }
+    fprintf(stderr,"Successfully bind!\n");
+    
+    /* クライアントからの接続要求を待つ */
+    if(listen(request_soc, gClientNum) == -1){
+		fprintf(stderr,"Cannot listen\n");
+		close(request_soc);
+		return -1;
+    }
+    fprintf(stderr,"Listen OK\n");
+
+    /* クライアントと接続する */
+    maxfd = MultiAccept(request_soc, gClientNum);
+    close(request_soc);
+    if(maxfd == -1)return -1;
+
+    /* 全クライアントの全ユーザー名を送る */
+    SendAllName();
+
+    /* select()のためのマスク値を設定する */
+    SetMask(maxfd);
+
+    return 0;
+}
+
+int SendRecvManager(void)
+{
+    char	command;
+    fd_set	readOK;
+    int		i;
+    int		endFlag = 1;
+    readOK = gMask;
+    /* クライアントからデータが届いているか調べる */
+    if(select(gWidth,&readOK,NULL,NULL,NULL) < 0){
+        /* エラーが起こった */
+        return endFlag;
+    }
+    for(i=0;i<gClientNum;i++){
+		if(FD_ISSET(gClients[i].fd,&readOK)){
+	    	/* クライアントからデータが届いていた */
+	    	/* コマンドを読み込む */
+			RecvData(i,&command,sizeof(char));
+	    	/* コマンドに対する処理を行う */
+	    	endFlag = ExecuteCommand(command,i);
+	    	if(endFlag == 0)break;
+		}
+    }
+    return endFlag;
+}
+int GetClientNum(void)
+{
+    return gClientNum;
+}
+
+int RecvIntData(int pos,int *intData)
+{
+    int n,tmp;
+    /* 引き数チェック */
+    assert(0<=pos && pos<gClientNum);
+    assert(intData!=NULL);
+    n = RecvData(pos,&tmp,sizeof(int));
+    (*intData) = ntohl(tmp);
+    return n;
+}
+
+void SendData(int pos,void *data,int dataSize)
+{
+    int	i;
+    /* 引き数チェック */
+    assert(0<=pos && pos<gClientNum || pos==ALL_CLIENTS);
+    assert(data!=NULL);
+    assert(0<dataSize);
+
+    if(pos == ALL_CLIENTS){
+    	/* 全クライアントにデータを送る */
+		for(i=0;i<gClientNum;i++){
+			write(gClients[i].fd,data,dataSize);
+		}
+    }
+    else{
+		write(gClients[pos].fd,data,dataSize);
+    }
+}
+
+void Ending(void)
+{
+    int	i;
+    printf("... Connection closed\n");
+    for(i=0;i<gClientNum;i++)close(gClients[i].fd);
+}
+
+static int MultiAccept(int request_soc,int num)
+{
+    int	i,j;
+    int	fd;
+    for(i=0;i<num;i++){
+		if((fd = accept(request_soc,NULL,NULL)) == -1){
+			fprintf(stderr,"Accept error\n");
+			for(j=i-1;j>=0;j--)close(gClients[j].fd);
+			return -1;
+		}
+		Enter(i,fd);
+    }
+    return fd;
+}
+
+static void Enter(int pos, int fd)
+{
+	/* クライアントのユーザー名を受信する */
+	read(fd,gClients[pos].name,MAX_NAME_SIZE);
+#ifndef NDEBUG
+	printf("%s is accepted\n",gClients[pos].name);
+#endif
+	gClients[pos].fd = fd;
+}
+static void SetMask(int maxfd)
+{
+    int	i;
+    gWidth = maxfd+1;
+    FD_ZERO(&gMask);    
+    for(i=0;i<gClientNum;i++)FD_SET(gClients[i].fd,&gMask);
+}
+
+static void SendAllName(void)
+{
+  int	i,j,tmp1,tmp2;
+    tmp2 = htonl(gClientNum);
+    for(i=0;i<gClientNum;i++){
+		tmp1 = htonl(i);
+		SendData(i,&tmp1,sizeof(int));
+		SendData(i,&tmp2,sizeof(int));
+		for(j=0;j<gClientNum;j++){
+			SendData(i,gClients[j].name,MAX_NAME_SIZE);
+		}
+	}
+}
+
+static int RecvData(int pos,void *data,int dataSize)
+{
+    int n;
+    /* 引き数チェック */
+    assert(0<=pos && pos<gClientNum);
+    assert(data!=NULL);
+    assert(0<dataSize);
+    n = read(gClients[pos].fd,data,dataSize); 
+    return n;
+}
+
+int RecvCharData(int pos,char *charData)
+{
+    int n;
+    /* 引き数チェック */
+    assert(0<=pos && pos<gClientNum); 
+    assert(charData!=NULL);
+    
+    // RecvData はサーバー側のヘルパー関数
+    n = RecvData(pos, charData, sizeof(char)); 
+
+    return n;
+}
